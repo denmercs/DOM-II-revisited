e9446b899c43fd95e1431645ed0a52daa8b84741
denmercs <denmercs@gmail.com>
2019-07-09 18:19:17 -0500

    Done with challenge 1 and 2

--- collapse/index.js
+++ collapse/index.js
@@ -5,11 +5,18 @@ class Collapse {
     console.log("Collapse.constructor toggleElement:", toggleElement); //
     // Hint: Get any needed data attributes from the `toggleElement`
     // Remember your "this." syntax
+    this.toggleElement = toggleElement;
+
+    this.card = document.querySelector(".collapse");
+    this.toggleElement.addEventListener("click", this.select.bind(this));
+  }
+  select() {
+    this.card.classList.toggle("collapse");
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const collapseToggles = document.querySelectorAll(`[data-toggle="collapse"]`);
-
+  console.log(collapseToggles);
   collapseToggles.forEach(button => new Collapse(button));
 });
--- modal/index.js
+++ modal/index.js
@@ -5,6 +5,26 @@ class Modal {
     console.log("Modal.constructor toggleElement:", toggleElement); //
     // Hint: Get any needed data attributes from the `toggleElement`
     // Remember your "this." syntax
+    this.toggleElement = toggleElement;
+    this.data = this.toggleElement.dataset.toggle;
+    console.log(this.data);
+    this.modal = document.querySelector("#exampleModal");
+    this.closeBtn = document.querySelector('.btn-secondary');
+
+    this.toggleElement.addEventListener('click', this.open.bind(this));
+    this.closeBtn.addEventListener('click', this.close.bind(this));
+  }
+
+  open() {
+    this.modal.classList.toggle('show');
+    this.modal.style.display = "block";
+    this.modal.setAttribute("aria-modal", "true");
+    this.modal.setAttribute("aria-hidden", "false");
+  }
+
+  close() {
+    this.modal.setAttribute("aria-hidden", "true");
+    this.modal.style.display = "none";
   }
 }
 
